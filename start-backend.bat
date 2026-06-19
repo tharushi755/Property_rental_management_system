429ef157b6c0589e339073dcaa9777bed47b52bf
Pawani Nawodya <pawaninawodhya@gmail.com>
2026-06-20 02:27:14 +0530

    Revert "Add startup batch files for backend and frontend" This reverts commit 90f452cdc573b09a63692c6bb48f63a133f31610.

--- start-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title VilaStay Backend
-set JAVA_HOME=C:\Program Files\Java\jdk-25
-set PATH=C:\Program Files\apache-maven-3.9.14\bin;%PATH%
-cd /d "%~dp0property-rental-backend"
-echo Starting VilaStay Backend...
-mvn spring-boot:run
-pause
--- start-frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title VilaStay Frontend
-cd /d "%~dp0property-rental-frontend (2)\property-rental-frontend"
-echo Starting VilaStay Frontend...
-npm start
-pause
